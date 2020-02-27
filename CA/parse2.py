77bd4e52e89ff731d8a954cdcf46b45b6c0ae85c
Ned Bingham <eab242@cornell.edu>
2020-02-27 13:39:47 -0500

    addresses are now parsed, starting to work on positions, offices, and organizations

--- CA/parse2.py
+++ CA/parse2.py
@@ -23,7 +23,9 @@ def depth(addr1, addr2):
 
 word_names = set(["rusty", "hicks", "abbey", "albert", "allie", "amber", "andrew", "angel", "april", "april", "art", "august", "aurora", "autumn", "baldric", "barb", "bay", "bill", "bob", "booth", "brad", "brandy", "brook", "buck", "candy", "carol", "carole", "cat", "chad", "charity", "chase", "chip", "christian", "chuck", "clay", "cliff", "colt", "cricket", "crystal", "daisy", "dale", "dale", "dash", "dawn", "dean", "derrick", "destiny", "dick", "dixie", "dolly", "don", "dori", "dory", "dot", "earl", "ebony", "elle", "eve", "faith", "fanny", "faye", "fern", "flora", "frank", "gale", "gay", "gene", "ginger", "glen", "gore", "grace", "grant", "guy", "hale", "harmony", "harry", "hazel", "heath", "heather", "heaven", "henry", "holly", "hope", "hunter", "iris", "ivy", "ivy", "jack", "jade", "jean", "jenny", "jerry", "jersey", "jewel", "jimmy", "john", "josh", "joy", "june", "kitty", "lacy", "lance", "laurel", "lee", "lily", "lily", "marina", "mark", "mark", "marsh", "mason", "matt", "max", "maxim", "may", "may", "mcdonald", "melody", "mike", "miles", "milo", "misty", "nick", "norm", "olive", "opal", "oral", "pam", "pansy", "pat", "patience", "patsy", "patty", "pearl", "peg", "penny", "pepper", "peter", "petunia", "pierce", "poppy", "queen", "ralph", "randy", "ransom", "ray", "red", "reed", "rich", "rick", "river", "rob", "rock", "roger", "rose", "rowan", "ruth", "sally", "sandy", "scot", "shad", "shepherd", "skip", "sky", "sly", "stone", "sue", "summer", "summer", "tab", "tad", "tanner", "tara", "tiffany", "tom", "tony", "tucker", "violet", "wade", "ward", "warren", "will", "winter", "wren"])
 
-word_places = set(["county", "state", "city", "regional", "street", "office", "blvd"])
+word_places = set(["regional", "website"])
+
+html_attrs = set(["id", "class", "style", "lang", "xml:lang", "coords", "shape"])
 
 class ParseCA:
 	def __init__(self):
@@ -40,24 +42,14 @@ class ParseCA:
 		self.emails = {}
 		self.links = {}
 		self.office = {}
-		self.address = {}
+		self.street = {}
+		self.apt = {}
+		self.city = {}
 		self.fax = {}
 		self.eng = enchant.Dict("en_US")
 		self.eng.add("signup")
-		self.eng.add("california")
 		self.eng.add("iframe")
-		self.eng.add("sacramento")
 		self.eng.add("analytics")
-		self.eng.add("san")
-		self.eng.add("diego")
-		self.eng.add("francisco")
-		self.eng.add("mateo")
-		self.eng.add("los")
-		self.eng.add("angeles")
-		self.eng.add("dc")
-		self.eng.add("washington")
-		self.eng.add("fresno")
-		self.eng.add("ca")
 		
 		self.phone_codes = [("ABC", 2), ("DEF", 3), ("GHI", 4), ("JKL", 5), ("MNO", 6), ("PQRS", 7), ("TUV", 8), ("WXYZ", 9)]
 		self.results = {}
@@ -81,45 +73,62 @@ class ParseCA:
 
 	def extractLinks(self, elem, addr):
 		if '@' not in elem:
-			objs = re.findall(r'(?:(?:[a-z]+://)?[-A-Za-z0-9_]+(?:\.[-A-Za-z0-9_]+)+)?(?:/[-A-Za-z0-9 _]+)*(?:\.(?:html|py|php|jpg|jpeg|png|gif|txt|md)|/)?(?:#[-A-Za-z0-9_]+)?(?:\?(?:[-A-Za-z0-9_]+=[-A-Za-z0-9_ \"\']*)+)?', elem)
+			objs = re.finditer(r'(?:(?:[a-z]+://)?[-A-Za-z0-9_]+(?:\.[-A-Za-z0-9_]+)+)?(?:/[-A-Za-z0-9 _]+)*(?:\.(?:html|py|php|jpg|jpeg|png|gif|txt|md)|/)?(?:#[-A-Za-z0-9_]+)?(?:\?(?:[-A-Za-z0-9_]+=[-A-Za-z0-9_ \"\']*)+)?', elem)
+			rng = []
+			start = 0
 			for obj in objs:
-				if obj:
-					obj = obj.lower()
-					if obj in self.links:
-						self.links[obj].append(addr)
+				link = obj.group(0).lower()
+				if link:
+					if link in self.links:
+						self.links[link].append(addr)
 					else:
-						self.links[obj] = [addr]
+						self.links[link] = [addr]
+				rng.append((start, obj.start(0)))
+				start = obj.end(0)
+			return ''.join([elem[s:e] for s, e in rng] + [elem[start:]])
+		else:
+			return elem
 
 	def extractEmails(self, elem, addr):
-		objs = re.findall(r'[A-Za-z.0-9]+@[A-Za-z]+.[A-Za-z]+', elem)
+		objs = re.finditer(r'[A-Za-z.0-9]+@[A-Za-z]+.[A-Za-z]+', elem)
+		rng = []
+		start = 0
 		for obj in objs:
-			obj = obj.lower()
-			sp = obj.split("@")
+			email = obj.group(0).lower()
+			sp = email.split("@")
 			sp[0] = sp[0].replace('.', '')
-			obj = '@'.join(sp)
-			if obj in self.emails:
-				self.emails[obj].append(addr)
+			email = '@'.join(sp)
+			if email in self.emails:
+				self.emails[email].append(addr)
 			else:
-				self.emails[obj] = [addr]
+				self.emails[email] = [addr]
+			rng.append((start, obj.start(0)))
+			start = obj.end(0)
+		return ''.join([elem[s:e] for s, e in rng] + [elem[start:]])
 
 	def extractPhones(self, elem, addr):
-		objs = re.findall(r'\(?[0-9][0-9][0-9]\)?[ -.][0-9][0-9][0-9][ -.][0-9A-Z][0-9A-Z][0-9A-Z][0-9A-Z]', elem)
+		objs = re.finditer(r'\(?[0-9][0-9][0-9]\)?[ -.][0-9][0-9][0-9][ -.][0-9A-Z][0-9A-Z][0-9A-Z][0-9A-Z]', elem)
+		rng = []
+		start = 0
 		for obj in objs:
-			obj = obj.replace("(", "").replace(")", "").replace(" ", "-").replace(".", "-")
+			phone = obj.group(0).replace("(", "").replace(")", "").replace(" ", "-").replace(".", "-")
 			for key, value in self.phone_codes:
 				for char in key:
-					obj = obj.replace(unicode(char), unicode(value))
+					phone = phone.replace(unicode(char), unicode(value))
 
 			if "fax" in elem.lower():
-				if obj in self.fax:
-					self.fax[obj].append(addr)
+				if phone in self.fax:
+					self.fax[phone].append(addr)
 				else:
-					self.fax[obj] = [addr]
+					self.fax[phone] = [addr]
 			else:
-				if obj in self.phones:
-					self.phones[obj].append(addr)
+				if phone in self.phones:
+					self.phones[phone].append(addr)
 				else:
-					self.phones[obj] = [addr]
+					self.phones[phone] = [addr]
+			rng.append((start, obj.start(0)))
+			start = obj.end(0)
+		return ''.join([elem[s:e] for s, e in rng] + [elem[start:]])
 
 	def extractNames(self, elem, addr):
 		objs = re.findall(ur'(?:[A-Z\u00C0-\u00D6\u00D8-\u00DE][a-z\u00DF-\u00f6\u00f8-\u00ff]+(?:-\s*[A-Z\u00C0-\u00D6\u00D8-\u00DE][a-z\u00DF-\u00f6\u00f8-\u00ff]+)*,\s*)?(?:[A-Z\u00C0-\u00D6\u00D8-\u00DE][a-z\u00DF-\u00f6\u00f8-\u00ff]+|[A-Z\u00C0-\u00D6\u00D8-\u00DE][A-Z\u00C0-\u00D6\u00D8-\u00DE])(?:\s[A-Z\u00C0-\u00D6\u00D8-\u00DE][a-z\u00DF-\u00f6\u00f8-\u00ff]+(?:-\s*[A-Z\u00C0-\u00D6\u00D8-\u00DE][a-z\u00DF-\u00f6\u00f8-\u00ff]+)*|\s[A-Z\u00C0-\u00D6\u00D8-\u00DE][a-z\u00DF-\u00f6\u00f8-\u00ff]*\.)*', elem)
@@ -146,54 +155,115 @@ class ParseCA:
 					self.names[obj].append(addr)
 				else:
 					self.names[obj] = [addr]
-				
+	
+	def extractPositions(self, elem, addr):
+		r_pos = r'Chair|Vice|Treasurer|Secretary|Executive|Director|Administrator|Fellow|Congressman|Congresswoman|Auditor|Leader|Senator|Representative|Member|Governer|Secretary|Controller|General|Attorney|Commissioner|Superindendent'
+
+	def extractLocations(self, elem, addr):
+		r_loc = r'Office'
+
+	def extractOrganizations(self, elem, addr):
+		r_org = r'Democrats|Dems|Republicans|Repubs|Reps|Party|Board'
+
+	def extractCity(self, elem, addr):
+		r_zip = r'\b[0-9]{5}(?:-[0-9]{4})?\b'
+		r_city = r'(?:[A-Z][a-z.-]+ ?)+'
+		r_state = r'Alabama|Alaska|Arizona|Arkansas|California|Colorado|Connecticut|Delaware|Florida|Georgia|Hawaii|Idaho|Illinois|Indiana|Iowa|Kansas|Kentucky|Louisiana|Maine|Maryland|Massachusetts|Michigan|Minnesota|Mississippi|Missouri|Montana|Nebraska|Nevada|New Hampshire|New Jersey|New Mexico|New York|North Carolina|North Dakota|Ohio|Oklahoma|Oregon|Pennsylvania|Rhode Island|South Carolina|South Dakota|Tennessee|Texas|Utah|Vermont|Virginia|Washington|West Virginia|Wisconsin|Wyoming'
+		r_stateabbr = r'AL|AK|AS|AZ|AR|CA|CO|CT|DE|D\.?C\.?|FM|FL|GA|GU|HI|ID|IL|IN|IA|KS|KY|LA|ME|MH|MD|MA|MI|MN|MS|MO|MT|NE|NV|NH|NJ|NM|NY|NC|ND|MP|OH|OK|OR|PW|PA|PR|RI|SC|SD|TN|TX|UT|VT|VI|VA|WA|WV|WI|WY'
+		
+		r_citystatezip = r_city + r',? (?:' + r_state + r'|' + r_stateabbr + r')(?: ' + r_zip + r')?'
+
+		objs = re.finditer(r_citystatezip, elem)
+		rng = []
+		start = 0
+		for obj in objs:
+			city = obj.group(0)
+			if city in self.city:
+				self.city[city].append(addr)
+			else:
+				self.city[city] = [addr]
+			rng.append((start, obj.start(0)))
+			start = obj.end(0)
+		return ''.join([elem[s:e] for s, e in rng] + [elem[start:]])
+	
+	def extractStreet(self, elem, addr):
+		r_street = r'(?:[0-9]+[A-Za-z]*|[A-Z][a-z]*) (?:[A-Z0-9][a-z0-9]*(?:-[A-Z0-9][a-z0-9]*)?\.? )+(?=Avenue|Lane|Road|Boulevard|Drive|Street|Mall|Plaza|Ave|Dr|Rd|Blvd|Ln|St)(?:Avenue|Lane|Road|Boulevard|Drive|Street|Mall|Plaza|Ave|Dr|Rd|Blvd|Ln|St)\.?'
+		r_po = r'P\.?O\.? Box [0-9]+'
+		r_streetbox = r'(?:' + r_po + '|' + r_street + ')'
+	
+		objs = re.finditer(r_streetbox, elem)
+		rng = []
+		start = 0
+		for obj in objs:
+			street = obj.group(0)
+			if street in self.street:
+				self.street[street].append(addr)
+			else:
+				self.street[street] = [addr]
+			rng.append((start, obj.start(0)))
+			start = obj.end(0)
+		return ''.join([elem[s:e] for s, e in rng] + [elem[start:]])
+
+	def extractApt(self, elem, addr):
+		r_apt = r'(?:[A-Z0-9][a-z0-9]*(?:-[A-Z0-9][a-z0-9]*)?\.?,? )*(?=Apartment|Building|Floor|Suite|Unit|Room|Department|Apt|Bldg|Fl|Ste|Rm|Dept)(?:Apartment|Building|Floor|Suite|Unit|Room|Department|Mall|Apt|Bldg|Fl|Ste|Rm|Dept)s?\.?'
+		r_aptnum = r'(:?[0-9]+[A-Za-z]* ' + r_apt + r'|' + r_apt + r' [0-9]+[A-Za-z]*(?:(?: *[-,&] *| +)[0-9]+[A-Za-z]*)+)'
+		
+		objs = re.finditer(r_aptnum, elem)
+		rng = []
+		start = 0
+		for obj in objs:
+			apt = obj.group(0)
+			if apt in self.apt:
+				self.apt[apt].append(addr)
+			else:
+				self.apt[apt] = [addr]
+			rng.append((start, obj.start(0)))
+			start = obj.end(0)
+		return ''.join([elem[s:e] for s, e in rng] + [elem[start:]])
+
+	def extractAttr(self, key, value, addr):
+		if value:
+			value = value.strip()
+			if value and key not in html_attrs:
+				value = self.extractPhones(value, addr)
+				value = self.extractEmails(value, addr)
+				if key in ["href", "src"]:
+					value = self.extractLinks(value, addr)
+				value = self.extractCity(value, addr)
+				value = self.extractStreet(value, addr)
+				value = self.extractApt(value, addr)
+				self.extractNames(value, addr)
+
+	def extractStr(self, elem, addr):
+		elem = elem.strip().replace(u'\xa0', u' ')
+		if elem:
+			elem = self.extractPhones(elem, addr)
+			elem = self.extractEmails(elem, addr)
+			elem = self.extractCity(elem, addr)
+			elem = self.extractStreet(elem, addr)
+			elem = self.extractApt(elem, addr)
+			self.extractNames(elem, addr)
 
 	def traverse(self, elem, addr=[]):
-		if isinstance(elem, Tag) and not isinstance(elem, Script):
+		if isinstance(elem, Tag) and elem.name not in html_attrs:
 			for key, value in elem.attrs.items():
-				if value:
-					value = value.strip()
-					if value:
-						self.extractEmails(value, addr)
-						if key in ["href", "src"]:
-							self.extractLinks(value, addr)
-						self.extractPhones(value, addr)
-						self.extractNames(value, addr)
+				self.extractAttr(key, value, addr)
 
 			stored = u''
 			for i, item in enumerate(elem.content):
 				if isinstance(item, unicode):
 					stored += item
 				else:
-					stored = stored.strip().replace(u'\xa0', u' ')
-					if stored:
-						self.extractEmails(stored, addr)
-						self.extractPhones(stored, addr)
-						self.extractNames(stored, addr)
-						stored = u''
+					self.extractStr(stored, addr)
+					stored = u''
 					self.traverse(item, addr + [i])
-			stored = stored.strip().replace(u'\xa0', u' ')
-			if stored:
-				self.extractEmails(stored, addr)
-				self.extractPhones(stored, addr)
-				self.extractNames(stored, addr)
-				stored = u''
+			self.extractStr(stored, addr)
+			stored = u''
 		elif isinstance(elem, STag):
 			for key, value in elem.attrs.items():
-				if value:
-					value = value.strip()
-					if value:
-						self.extractEmails(value, addr)
-						if key in ["href", "src"]:
-							self.extractLinks(value, addr)
-						self.extractPhones(value, addr)
-						self.extractNames(value, addr)
+				self.extractAttr(key, value, addr)
 		elif isinstance(elem, unicode):
-			elem = elem.strip()
-			if elem:
-				self.extractEmails(elem, addr)
-				self.extractPhones(elem, addr)
-				self.extractNames(elem, addr)
+			self.extractStr(stored, addr)
 
 	def findBest(self, best, addr, data):
 		for _, tests in data.items():
@@ -203,7 +273,7 @@ class ParseCA:
 					best = addr[0:d]
 		return best
 
-	def buildEntry(self, entry, addr, start, end, data, key):
+	def buildEntry(self, entry, addr, start, end, data, key, clear=True):
 		for item, tests in data.items():
 			found = False
 			for test in tests:
@@ -216,6 +286,8 @@ class ParseCA:
 					entry[key].add(item)
 				else:
 					entry[key] = set([item])
+				if clear:
+					del data[item]
 		return entry
 
 	def develop(self):
@@ -258,11 +330,12 @@ class ParseCA:
 				entry = self.buildEntry(entry, base, start, end, self.phones, 'phone')
 				entry = self.buildEntry(entry, base, start, end, self.links, 'link')
 				entry = self.buildEntry(entry, base, start, end, self.fax, 'fax')
+				entry = self.buildEntry(entry, base, start, end, self.city, 'city', False)
+				entry = self.buildEntry(entry, base, start, end, self.street, 'street', False)
+				entry = self.buildEntry(entry, base, start, end, self.apt, 'apt', False)
 
 			if name in self.results:
-				print entry
 				old = self.results[name]
-				print old
 				if 'email' in old and 'email' in entry:
 					entry['email'] |= set(old['email'])
 				elif 'email' in old:
@@ -283,6 +356,22 @@ class ParseCA:
 				elif 'link' in old:
 					entry['link'] = set(old['link'])
 
+				if 'city' in old and 'city' in entry:
+					entry['city'] |= set(old['city'])
+				elif 'city' in old:
+					entry['city'] = set(old['city'])
+
+				if 'street' in old and 'street' in entry:
+					entry['street'] |= set(old['street'])
+				elif 'street' in old:
+					entry['street'] = set(old['street'])
+
+				if 'apt' in old and 'apt' in entry:
+					entry['apt'] |= set(old['apt'])
+				elif 'apt' in old:
+					entry['apt'] = set(old['apt'])
+
+
 			if 'email' in entry:			
 				entry['email'] = list(entry['email'])
 			if 'phone' in entry:
@@ -291,14 +380,26 @@ class ParseCA:
 				entry['fax'] = list(entry['fax'])
 			if 'link' in entry:
 				entry['link'] = list(entry['link'])
+			if 'city' in entry:
+				entry['city'] = list(entry['city'])
+			if 'street' in entry:
+				entry['street'] = list(entry['street'])
+			if 'apt' in entry:
+				entry['apt'] = list(entry['apt'])
 
 			self.results[name] = entry
+		print 'emails: ' + repr(self.emails)
+		print 'phones: ' + repr(self.phones)
+		print 'links: ' + repr(self.links)
+		print 'fax: ' + repr(self.fax)
 		self.emails = {}
 		self.phones = {}
 		self.links = {}
 		self.names = {}
 		self.office = {}
-		self.address = {}
+		self.city = {}
+		self.street = {}
+		self.apt = {}
 
 	def scrape(self, url):
 		uid = str(len(self.urls))
@@ -308,11 +409,14 @@ class ParseCA:
 
 parser = ParseCA()
 
-parser.scrape("http://www.cadem.org/our-party/leaders")
-parser.scrape("http://www.cadem.org/our-party/elected-officials")
-parser.scrape("http://www.cadem.org/our-party/our-county-committees")
+#parser.scrape("http://www.cadem.org/our-party/leaders")
+#parser.scrape("http://www.cadem.org/our-party/elected-officials")
+#parser.scrape("http://www.cadem.org/our-party/our-county-committees")
 #parser.scrape("http://www.cadem.org/our-party/dnc-members")
 #parser.scrape("https://nydems.org/our-party/")
+#parser.scrape("https://missouridemocrats.org/county-parties/")
+parser.scrape("https://missouridemocrats.org/officers-and-staff/")
+
 
 
 print json.dumps(parser.results, indent=2)
@@ -324,6 +428,7 @@ print json.dumps(parser.results, indent=2)
 # separate out names
 # get bound from names
 # fall back to findBest
+# change findBest to prioritise distance
 # develop
 # check name mergers
 
